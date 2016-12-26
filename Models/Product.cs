64fe3b27371bec71cd8f7fb20e9ece67cf0bb35c
bsokat <barissokat@gmail.com>
2016-12-26 20:19:22 +0300

    Model class added

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetAllProducts_WebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
